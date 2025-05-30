c14b8eafff1d509f1ec2b192bccee08133ac1ae8
Eliana-Giselle-Miwszuk <eligise@outlook.com>
2025-05-30 17:14:08 -0300

    arreglo un error

--- Tp-webpromo-equipo-8B/Default.aspx.designer.cs
+++ Tp-webpromo-equipo-8B/Default.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Tp_webpromo_equipo_8B
 
     public partial class Default
     {
+
+        /// <summary>
+        /// Control btnVoucher.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVoucher;
     }
 }
